65a23173facf08a4f384d0885bfeeb8f1f40f5b5
remmy hume <rem@remics.net>
2013-12-18 12:27:24 +0100

    changed notification timer so that it just runs when there is at least one notification in queue

--- data/js/notification.js
+++ data/js/notification.js
@@ -1,46 +1,53 @@
 var notification = {
 
-_queue: [],
+  _queue: [],
 
-_delay: 3000,
+  _delay: 3000,
 
-_k_limit: 3,
+  _k_limit: 3,
 
-init:
-function init() {
-    notification.check_proc();
-    setInterval(notification.check_proc, 1000);
-},
-
-check_proc:
-function check_proc() {
+  check_proc: function check_proc() {
     if (notification._queue.length) {
-        var tuple = notification._queue.shift();
-        notification.notify(tuple[0], tuple[1], tuple[2], tuple[3]);
+      var tuple = notification._queue.shift();
+      notification.notify(tuple[0], tuple[1], tuple[2], tuple[3]);
+      if (!notification._queue.length) {
+        clearInterval(notification.notificationTimer);
+        notification.notificationTimer = null;
+      }
     }
-},
+  },
 
-notify:
-function notify(title, summary, image, type) {
+  notificationTimer: null,
+
+  startTimer: function () {
+    notification.notificationTimer = setInterval(notification.check_proc, 100);
+  },
+
+  notify: function (title, summary, image, type) {
     title = title.replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&');
     summary = summary.replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&');
     if (util.is_native_platform()) {
-        hotot_action('system/notify/'
-            + type
-            + '/' + encodeURIComponent(title)
-            + '/' + encodeURIComponent(summary)
-            + '/' + encodeURIComponent(image));
+      hotot_action('system/notify/' + type + '/' + encodeURIComponent(title) + '/' + encodeURIComponent(summary) + '/' + encodeURIComponent(image));
     } else if (conf.vars.platform == 'Chrome') {
-        var img_url = image? image: './image/ic64_hotot.png';
-        var notification = webkitNotifications.createNotification(img_url, title, summary);
-        notification.show();
-        setTimeout(function() {notification.cancel()}, 5000);
+      var img_url = image ? image : './image/ic64_hotot.png';
+      var notification = webkitNotifications.createNotification(img_url, title, summary);
+      notification.show();
+      setTimeout(function () {
+        notification.cancel()
+      }, 5000);
     }
-},
+  },
 
-push:
-function push(title, summary, image, type) {
+  push: function (title, summary, image, type) {
     notification._queue.push([title, summary, image, type]);
-}
+    if (!notification.notificationTimer) {
+      notification.check_proc();
+      notification.notificationTimer = notification.timerFunction;
+    }
+  },
+
+  init: function () {
+    notification.check_proc();
+  }
 
 };
